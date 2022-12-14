8f1e3dd15a15965f55cbb7b94bc5fe72711c46d7
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2022-12-14 14:01:20 +0100

    primer commit jorge

--- src/Main.java
+++ src/Main.java
@@ -10,4 +10,9 @@ public class Main {
     //A parte  somos jóvenes y nos queda toda la vida por delante para reir.
 
     //Voy a reir, voy a bailar vivir vivir la la la la laaaaa
+
+  //Angi y LUycia son las mejores compañeras
+    //Dan consejos y ayudan a cualquiera
+    //Aunque a veces Angi este arrecha
+    //SIempre esta ahi detras y lucia a mi izquierda
 }
